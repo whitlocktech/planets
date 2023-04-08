f4b8191baba8236cb7356488b6df5bae862d01e9
whitlocktech <whitlocktech@gmail.com>
2023-04-08 11:56:21 -0500

    Planets ZTM course first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { parse } = require('csv-parse')
+const fs = require('fs')
+
+const habitablePlanets = []
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] <1.6
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({ //Sets the comments to the # and then the columns set to true. // pipe takes the readable stream and pushes it to the writeable array.
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data)}
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name']
+        }))
+        console.log(`${habitablePlanets.length} Potentially habitable planets found!`)
+        console.log('done')
+    })
+
